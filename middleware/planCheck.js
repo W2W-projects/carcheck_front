3140a64fba2312fcbd82f614fe7082ecad193758
Ram W2W <ramw2w@Rams-MacBook-Pro.local>
2025-02-18 12:28:06 +0000

    updated plan middleware

--- middleware/planCheck.js
+++ middleware/planCheck.js
@@ -2,7 +2,7 @@ import { usePlanStore } from "~/stores/plan";
 
 export default defineNuxtRouteMiddleware((to, next)=>{
     const planStore = usePlanStore();
-    debugger
+
     if(!planStore.getSelectedPlan){
         return navigateTo("/payment/plans");
     }
